37a8007486ca8889b665524e2a59be1234144d28
Sam Kennerly <samkennerly@gmail.com>
2017-02-19 00:19:54 -0500

    moved old code to ARCHIVE, finished demo notebooks, fixed alphabet glitch

--- Fidelio_Functions.py
+++ Fidelio_Functions.py
@@ -17,10 +17,10 @@ ALPHABET42 = ('0','1','2','3','4','5','6','7','8','9',  \
             ' ','A','B','C','D','E','F','G','H','I',    \
             'J','K','L','M','N','O','P','Q','R','S',    \
             'T','U','V','W','X','Y','Z',',','.','?',    \
-            '!',' ')
+            '!','/')
 
-# ASCII chars 32-127 (0-31 are special system commands)
-ALPHABET96 = tuple([ chr(i) for i in range(32,128) ])
+# Default alphabet is ASCII chars 32 through 125
+ALPHABET94 = tuple([ chr(i) for i in range(32,126) ])
 
 # Where did we save the list of prime numbers?
 PRIMES_FILE     = 'Primes.txt'
@@ -29,42 +29,10 @@ PRIMES_FILE     = 'Primes.txt'
 DISCARD_PRIMES  = 1000
 
 
-# Functions for mapping text <-> [list of integers]
-
-def char_to_num(alphabet):
-    ''' Make a dictionary with chars as keys and ints as values '''
-
-    char_dict = dict.fromkeys(alphabet)
-    for k in range(len(alphabet)):
-        char_dict[ alphabet[k] ] = k
-
-    return char_dict
-
-def text_to_digits(text,alphabet=ALPHABET96):
-    '''
-    Transform a string into a list of ints.
-    Caution: characters not in the alphabet will be dropped!
-    '''
-
-    char_dict   = char_to_num(alphabet)
-    digits      = [ char_dict[x] for x in text if x in char_dict.keys() ]
-
-    return digits
-
-def digits_to_text(digits,alphabet=ALPHABET96):
-    ''' Transform a list of ints into a string '''
-
-    too_big = len(alphabet)
-    text    = [ alphabet[x] for x in digits if x < too_big ]
-    text    = "".join(text)
-
-    return text
-
-
 
 # Simple encryption functions
 
-def caesar(plain_text,shift=3,decrypt=False,alphabet=ALPHABET96):
+def caesar(plain_text,shift=-3,decrypt=False,alphabet=ALPHABET94):
     '''
     Caesar cipher: shift all characters by the same amount
     using modular arithmetic. Default is classic Caesar.
@@ -80,7 +48,7 @@ def caesar(plain_text,shift=3,decrypt=False,alphabet=ALPHABET96):
 
     return cipher_text
 
-def dodgson(plain_text,password,decrypt=False,alphabet=ALPHABET96):
+def dodgson(plain_text,password,decrypt=False,alphabet=ALPHABET94):
 
     digits      = text_to_digits(plain_text,alphabet)
     passcode    = text_to_digits(password,alphabet)
@@ -105,7 +73,66 @@ def dodgson(plain_text,password,decrypt=False,alphabet=ALPHABET96):
 
 
 
-# Functions for packing and padding a list of 2-digit ints
+# RSA encryption functions
+
+def generate_keys(too_small=10000,too_large=100000,verbose=False):
+    ''' Generate RSA number, public key, and private key '''
+
+    primes      = load_primes(too_small,too_large)
+    n, totient  = choose_rsa_number(primes,verbose)
+    public_key  = choose_public_key(primes,totient,verbose)
+    private_key = find_private_key(public_key,totient,verbose)
+
+    return n, public_key, private_key
+
+def rsa_encrypt(message,n,public_key,alphabet=ALPHABET94):
+    ''' Encrypt a message using RSA '''
+
+    digits  = packetize(text_to_digits(message,alphabet))
+    cipher  = [ pow(m,public_key,n) for m in digits ]
+
+    return cipher
+
+def rsa_decrypt(cipher,n,private_key,alphabet=ALPHABET94):
+    ''' Decrypt an RSA cipher '''
+
+    decipher    = [ pow(c,private_key,n) for c in cipher ]
+    plaintext   = digits_to_text(unpacketize(decipher),alphabet)
+
+    return plaintext
+
+
+
+# Functions for mapping text <-> [list of integers]
+
+def char_to_num(alphabet):
+    ''' Make a dictionary with chars as keys and ints as values '''
+
+    char_dict = dict.fromkeys(alphabet)
+    for k in range(len(alphabet)):
+        char_dict[ alphabet[k] ] = k
+
+    return char_dict
+
+def text_to_digits(text,alphabet=ALPHABET94):
+    '''
+    Transform a string into a list of ints.
+    Caution: characters not in the alphabet will be dropped!
+    '''
+
+    char_dict   = char_to_num(alphabet)
+    digits      = [ char_dict[x] for x in text if x in char_dict.keys() ]
+
+    return digits
+
+def digits_to_text(digits,alphabet=ALPHABET94):
+    ''' Transform a list of ints into a string '''
+
+    too_big = len(alphabet)
+    text    = [ alphabet[x] for x in digits if x < too_big ]
+    text    = "".join(text)
+
+    return text
 
 def packetize(digits):
     '''
@@ -177,74 +204,8 @@ def unpacketize(packets):
     return digits
 
 
-# Maths for RSA
 
-def gcd(x,y):
-    '''
-    Find the greatest common denominator of two positive ints.
-    (This is a recursive version of the Euclidean Algorithm.)
-    '''
-    if y == 0:
-        return x
-    return gcd(y,x%y)
-
-def gcd_factor(e,n,verbose=False):
-    '''
-    Given two positive integers e and n with e < n, the 
-    Extended Euclidean Algorithm finds x,y such that
-    nx + ey = gcd(e,n). This is a limited version which finds
-    gcd(e,n) and x, but doesn't bother finding y.
-    '''
-    
-    assert e < n, "First arg should be smaller than second arg!"
-    
-    r = (n,e)
-    x = (0,1)
-    while r[1] > 0:
-        if verbose:
-            print( r[0], x[0] )
-        quotient = r[0] // r[1]
-        next_r = r[0] - quotient * r[1]
-        next_x = x[0] - quotient * x[1]
-        r = ( r[1], next_r )
-        x = ( x[1], next_x )
-        
-    # We want the results just before the last row
-    gcd = r[0]
-    x   = x[0]
-    
-    # If x is negative, then replace it with a positive
-    # number which is equivalent to x (mod n).
-    if x < 0:
-        x = x % n
-        
-    return gcd, x
-
-def test_gcd_factor(e,n,verbose=False):
-    '''
-    It's remarkably easy to screw up the Extended Euclidean Algorithm.
-    Use this to verify that gcd_factor() does what I think it does.
-    '''
-    
-    gcd_test, x_test = gcd_factor(e,n)
-        
-    # Did we get the correct GCD?
-    gcd_true = gcd(n,e)
-    if gcd_test != gcd_true :
-        print( "GCD of %s and %s is %s, not %s" % (e,n,gcd_true,gcd_test) )
-        return False
-    
-    # Does (e * x) % n = gcd(e,n) ?
-    product_test = (e*x_test) % n
-    if product_test != gcd_true:
-        print( "%s times %s mod %s is %s, not %s" % (e,x_test,n,product_test,gcd_true) )
-        return False
-    
-    return True
-
-
-
-# RSA functions
+# RSA utility functions
 
 def load_primes(too_small=None,too_large=None):
     '''
@@ -276,7 +237,7 @@ def choose_rsa_number(primes,verbose=False):
 
     if verbose:
         print( "RSA number is %s * %s = %s" % (p,q,n) )
-        print( "Euler totient is %s" % totient )
+        print( "Euler totient is %s * %s = %s" % (p-1,q-1,totient) )
 
     return n, totient
 
@@ -290,14 +251,12 @@ def choose_public_key(primes,totient,verbose=False):
         
         [public_key] = random.sample(primes,1)
 
-        if verbose:
-            print( "Testing public key %s" % public_key )
-
         if (public_key >= totient) | (totient % public_key ==0):
             continue
 
         else:
-            print( "Public key is %s" % public_key )
+            if verbose:
+                print( "Public key is %s" % public_key )
             return public_key
 
     # Hopefully we never get this far
@@ -310,7 +269,7 @@ def find_private_key(public_key,totient,verbose=False):
     Throw an exception if that happens.
     '''
 
-    gcd, private_key = gcd_factor(public_key,totient)
+    gcd, private_key = gcd_and_inverse(public_key,totient)
     assert (gcd == 1), "GCD of public key %s and totient %s is %s, not 1" % (public_key,totient,gcd)
 
     if verbose:
@@ -318,109 +277,72 @@ def find_private_key(public_key,totient,verbose=False):
 
     return private_key
 
-def generate_keys(too_small=10000,too_large=100000,verbose=False):
-    ''' Generate RSA number, public key, and private key '''
-
-    primes      = load_primes(too_small,too_large)
-    n, totient  = choose_rsa_number(primes,verbose)
-    public_key  = choose_public_key(primes,totient,verbose)
-    private_key = find_private_key(public_key,totient,verbose)
-
-    return n, public_key, private_key
 
 
+# Maths for RSA
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def generatekey():
+def gcd(x,y):
     '''
-    Generate an RSA number, totient, public and private key
+    Find the greatest common denominator of two positive ints.
+    (This is a recursive version of the Euclidean Algorithm.)
     '''
+    if y == 0:
+        return x
+    return gcd(y,x%y)
 
-    primesfile = open("10000primes.txt", "r")
-    primeslist = primesfile.read().split()
-    prime0 = int(primeslist[random.randint(5000,len(primeslist))])  # The first 5000 primes are not used.   
-    prime1 = int(prime0)
-
-    while prime1 == prime0 :        # This loop ensures that the two primes are not the same.
-        prime1 = int(primeslist[random.randint(5000,len(primeslist))])
-    totient = (prime0 - 1) * (prime1 - 1)
-    rsan = prime0 * prime1
+def gcd_and_inverse(k,n,verbose=False):
+    '''
+    Given two positive integers k and n with k < n, the 
+    Extended Euclidean Algorithm finds x,y such that
+    nx + ky = gcd(k,n). This is a limited version which finds
+    gcd(k,n) and x, but it doesn't bother finding y.
+    '''
     
-    # Choose a (prime) public key that is large but less than the totient.
-    publickey = 10968163441         # This is the 10000th prime squared; totient is less than this
-    while publickey > totient:
-        publickey = int(primeslist[random.randint(5000,len(primeslist))])   # First 5000 primes not used.   
+    assert k < n, "First arg should be smaller than second arg!"
     
-    # <findinverse> finds the multiplicative inverse of the public key (mod totient).
-    privatekey = findinverse(publickey, totient)
-    RSAkeylist = [prime0, prime1, rsan, publickey, privatekey, totient]
+    r = (n,k)
+    x = (0,1)
+    while r[1] > 0:
+        if verbose:
+            print( r[0], x[0] )
+        quotient = r[0] // r[1]
+        next_r = r[0] - quotient * r[1]
+        next_x = x[0] - quotient * x[1]
+        r = ( r[1], next_r )
+        x = ( x[1], next_x )
+        
+    # We want the results just before last
+    gcd = r[0]
+    x   = x[0]
+    
+    # If x is negative, then replace it with a positive
+    # number which is equivalent to x (mod n).
+    if x < 0:
+        x = x % n
+        
+    return gcd, x
 
-    return RSAkeylist
-
-def RSAencrypt(packetlist, rsan, publickey):
+def test_gcd_and_inverse(k,n,verbose=False):
     '''
-    Encrypt a list of long ints using the RSA scheme.
+    It's easy to implement the Extended Euclidean Algorithm incorrectly.
+    Use this to verify that gcd_and_inverse() does what I think it does.
     '''
-
-    cipherlist = []
-    cipherpacket = ""
-    for i in range(len(packetlist)) :
-        cipherlist.append( modexp( packetlist[i], publickey, rsan ) )
-
-    return cipherlist
-
-def RSAdecrypt(cipherlist, rsan, privatekey):
-    ''' Decrypt result of RSAencrypt '''
-
-    decryptedlist = []
-    for i in range(len(cipherlist)) :
-        decryptedlist.append( modexp( cipherlist[i] , privatekey , rsan ) )
-
-    return decryptedlist
-
-def modexp(a, n, m):
-    '''
-    Fast modular exponentiation of large numbers
-    Author: Wojtek Jamrozy (www.wojtekrj.net)
-    '''
-
-    bits = []
-    while n:
-        bits.append(n%2)
-        n /= 2
-    solution = 1
-    bits.reverse()
-    for x in bits:
-        solution = (solution*solution)%m
-        if x:
-            solution = (solution*a)%m
-
-    return solution
-
-
+    
+    gcd_test, x_test = gcd_and_inverse(k,n)
+        
+    # Did we get the correct GCD?
+    gcd_true = gcd(n,k)
+    if gcd_test != gcd_true :
+        print( "GCD of %s and %s is %s, not %s" % (k,n,gcd_true,gcd_test) )
+        return False
+    
+    # Does (e * x) % n = gcd(e,n) ?
+    product_test = (k*x_test) % n
+    if product_test != gcd_true:
+        print( "%s times %s mod %s is %s, not %s" % (k,x_test,n,product_test,gcd_true) )
+        return False
+    
+    return True
 
 
 
